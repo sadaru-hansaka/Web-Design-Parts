c5b9e9cba9204f95d9b6ef7e715aafa21cc4c2b7
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-29 21:30:01 +0530

    did a experiment to rotate an image around the Y axxis and change the image when it rotate

---
+++ Rotatable Images/script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.image-container');
+    const image1 = document.getElementById("image1");
+    const image2 = document.getElementById("image2");
+
+    image1.style.display = 'block';
+    image2.style.display = 'block';
+ });
+
