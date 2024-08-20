62fa4a0ff23fcc24913108e1870a998c7151d12b
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-08-20 21:54:12 +0530

    Added a contact me page

--- Contact me page/script.js
+++ Contact me page/script.js
@@ -1,19 +1,31 @@
-function sendMail() {
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value,
-    }
+import {key} from './key.js';
 
-    //enter service key and template key
-    emailjs.send("service key", "template key", parms).then(
-        function(response) {
-            alert("Email sent successfully");
-            document.querySelector('.form').reset(); 
-        },
-        function(error) {
-            alert("Failed to send email");
-        }
+const form = document.querySelector('form');
+const name = document.getElementById('name');
+const mail = document.getElementById('email');
+const messege = document.getElementById('message');
+const subject = document.getElementById('subject');
+
+
+
+function sendEmail() {
+    const msg = `Name : ${name.value}<br> Email : ${mail.value}<br><br> ${messege.value}`
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "hansakas305@gmail.com",
+        Password : key,
+        To : 'hansakas305@gmail.com',
+        From : "hansakas305@gmail.com",
+        Subject : subject.value,
+        Body : msg,
+    }).then(
+      message => alert(message)
     );
 }
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    sendEmail();
+    this.reset();
+})
